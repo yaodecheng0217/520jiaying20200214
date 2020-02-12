698d792fffdc6d7667e33b8483ba32b8f015eafc
YaoDecheng <yaodecheng@outlook.com>
2020-02-12 12:06:14 +0800

    new

--- assets/js/post.js
+++
@@ -1,8 +0,0 @@
-var postContent = document.querySelector('#post-content');
-var imgs = postContent.querySelectorAll('img');
-var len = imgs.length;
-
-for (let i = 0; i < len; i++) {
-    imgs[i].classList.add('materialboxed');
-    imgs[i].classList.add('z-depth-4');
-}
